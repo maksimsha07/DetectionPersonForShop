9e778e296840c23fcc40eb8cb61c86da5b46b1ea
MaksimCh <cheshikhin.maksim@gmail.com>
2022-03-22 21:33:44 +0300

    first commit

---
+++ DetectionPersonForShop.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DetectionPersonForShop.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
