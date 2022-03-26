d027878aa8b6d50dc1e886d77f6e1c7c3aae9850
MaksimCh <cheshikhin.maksim@gmail.com>
2022-03-26 23:21:53 +0300

    Пересоздал проект vue на версии 2.x

--- detectionpersonforshop.front/src/main.js
+++ detectionpersonforshop.front/src/main.js
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import { BootstrapVue,IconsPlugin} from 'bootstrap-vue-3'
-import 'bootstrap'
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- detectionpersonforshop.front/src/router/index.js
+++ detectionpersonforshop.front/src/router/index.js
@@ -1,5 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -7,10 +10,17 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
   routes
 })
 
